8419f47d6cd68aacbce2fb7d638ed9634ce7db8c
Imee Cuison <imeetwelve@gmail.com>
2015-02-09 14:05:40 -0500

    first commit

---
+++ src/game.js
@@ -0,0 +1,11 @@
+Game = {
+  //Initiliaze and start our game
+
+  start: function () {
+    //Start crafty and set background color
+    Crafty.init(480, 320);
+    Crafty.background('green');
+
+  }
+
+}
